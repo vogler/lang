8d46900b960b3ca6c5971cc7fe170b3371b1c395
Ralf Vogler <ralf.vogler@gmail.com>
2019-10-17 11:37:43 +0200

    nonterm: while(true) is never, but while(1) is not

--- typescript/never.ts
+++ typescript/never.ts
@@ -8,5 +8,14 @@ const va: a = {id: 1, name: 'ha', foo: 'foo'}
 const vb: b = {id: '1', name: 'ha', bar: 'bar'}
 const vc1: c = va
 const vc2: c = vb
-const vc3/*: c*/ = { ...va, ...vb }
+const vc3/*: c*/ = { ...va, ...vb } // second field id overwrites first one
 const vd: d = vc3 // error message could be better; there is no value satisfying d since id: never
+
+const nonterm = (x: never) => nonterm(x) // (x: never) => any
+// inferred return type for both nonterm2 and nonterm3 is void, but for nonterm3 it unifies with never?!
+function nonterm2(): never { // Error: A function returning 'never' cannot have a reachable end point
+  while(1){}
+}
+function nonterm3(): never {
+  while(true){} // why does 1 above not work?
+}
